9006703c465eaa6baef290c17c238be2c67ec42b
Mateus Coury <mateuscoury@gmail.com>
2021-08-05 12:51:33 -0300

    Updates

---
+++ precache-manifest.fd09d1b3b98cc1aeb25d94d6a438ee9d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b1e342b30fe1654ddcb7d35870ac59",
+    "url": "/appReceitas/index.html"
+  },
+  {
+    "revision": "0920d8d9d382c4845579",
+    "url": "/appReceitas/static/css/2.17323d26.chunk.css"
+  },
+  {
+    "revision": "bccbcbe55261f5ba43e7",
+    "url": "/appReceitas/static/css/main.b6c73147.chunk.css"
+  },
+  {
+    "revision": "0920d8d9d382c4845579",
+    "url": "/appReceitas/static/js/2.1fe0d6ef.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/appReceitas/static/js/2.1fe0d6ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bccbcbe55261f5ba43e7",
+    "url": "/appReceitas/static/js/main.7fb14e30.chunk.js"
+  },
+  {
+    "revision": "bf072cde1bdab38531b6",
+    "url": "/appReceitas/static/js/runtime-main.84ed4e63.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/appReceitas/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "5399c99c24c1e5735ea0cf0982679f0e",
+    "url": "/appReceitas/static/media/cookie.5399c99c.svg"
+  },
+  {
+    "revision": "d38551a9ddd8c7f80e95b96f64c03744",
+    "url": "/appReceitas/static/media/do.d38551a9.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/appReceitas/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "55c60b0cd02d1eace809c6ff221e27ad",
+    "url": "/appReceitas/static/media/ea.55c60b0c.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/appReceitas/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "66634d99fc0cf4e5e5e079bba67f50d3",
+    "url": "/appReceitas/static/media/hat.66634d99.svg"
+  },
+  {
+    "revision": "5d5775127f232d6de9441ee409dea6a0",
+    "url": "/appReceitas/static/media/it.5d577512.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/appReceitas/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/appReceitas/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/appReceitas/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/appReceitas/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/appReceitas/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/appReceitas/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/appReceitas/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/appReceitas/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
